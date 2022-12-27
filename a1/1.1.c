e823a2370076bba7fa0144178e2b01a32a0a780c
SabeehSulehri <121465572+SabeehSulehri@users.noreply.github.com>
2022-12-27 01:43:29 +0100

    Add files via upload

---
+++ a1/1.1.c
@@ -0,0 +1,17 @@
+/*
+CH-230-A
+a1_p1.c
+Sabeeh Sulehri
+s.sulehri@jacobs-university.de
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    double result;
+    result = ((3.00 + 1.00) / 5.00);
+    printf("The value of 4/5 is %.2lf\n", result);
+
+    return 0;
+}
---
+++ a1/1.6.c
@@ -0,0 +1,16 @@
+/*
+CH-230-A
+a1_p6.c
+Sabeeh Sulehri
+s.sulehri@jacobs-university.de
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char c = 'F';
+    printf("%c\n%i",c+3,c+3);
+
+    return 0;
+}
